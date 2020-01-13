bf900341ab417715d85637bdf5d71e247d4bd944
1F1ERCE1 <helmichcurran@gmail.com>
2020-01-12 21:15:56 -0600

    First!

---
+++ The Limping Dog Pre-Alpha/Assets/MarksaBasicB.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarksaBasicB : MonoBehaviour
+{
+    float charSpeed = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * charSpeed); //Moves Character Forward and Back
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * charSpeed);
+        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X"));
+    }
+}
