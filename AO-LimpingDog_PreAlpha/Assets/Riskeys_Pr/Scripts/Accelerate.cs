d244e5e6bf92ec9d840293e13aabeb039aa578f9
RISKEYDERPPY <cristnickolaus@gmail.com>
2020-05-28 08:49:55 -0700

    Made Little SexyProject

---
+++ AO-LimpingDog_PreAlpha/Assets/Riskeys_Pr/Scripts/Accelerate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerate : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+
+    [SerializeField]
+    KeyCode keyPositive;
+    [SerializeField]
+    KeyCode keyNegative;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3Force;
+
+        if (Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3Force;
+    }
+}
---
+++ AO-LimpingDog_PreAlpha/Assets/Riskeys_Pr/Scripts/Velocity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity += v3Force;
+    }
+}
